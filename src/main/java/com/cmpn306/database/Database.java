667b77d6baa5b248391228ebc1a5047b72dddfd3
Ziyad <ziyad.ss@hotmail.com>
2022-05-17 22:09:28 +0200

    Fixed database empty statement bug
    Co-Authored-By: Khaled Ibrahim <65508021+Huchra@users.noreply.github.com>
    Co-Authored-By: Ahmed Ayman Saad <34175435+AhmedAymanSaad@users.noreply.github.com>

--- src/main/java/com/cmpn306/database/Database.java
+++ src/main/java/com/cmpn306/database/Database.java
@@ -3,36 +3,29 @@ package com.cmpn306.database;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Database {
-    String     schemaPath   = "./schema.sql";
-    String     databaseName = "searchEngineDatabase";
-    Connection connection;
+    private final String     SCHEMA_PATH   = "./schema.sql";
+    private final String     DATABASE_NAME = "searchEngineDatabase";
+    private final Connection connection;
 
-    Database() throws SQLException, IOException {
-        connectDatabase();
+    public Database() throws SQLException, IOException {
+        connection = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_NAME);
         createTables();
-
-    }
-
-    void connectDatabase() throws SQLException {
-        connection = DriverManager.getConnection("jdbc:sqlite:" + databaseName);
     }
 
     void createTables() throws IOException, SQLException {
-        Path      filePath = Path.of(schemaPath);
-        String    sql      = Files.readString(filePath);
-        String[]  tokens   = sql.split(";");
-        Statement stmt     = connection.createStatement();
+        Statement stmt = connection.createStatement();
+
+        String   sql    = Files.readString(Path.of(SCHEMA_PATH));
+        String[] tokens = sql.split(";");
+
         for (String token: tokens) {
-            //token = token + ";";
-            System.out.println(token);
-            stmt.addBatch(token);
+            if (!token.isBlank())
+                stmt.addBatch(token);
         }
+        
         stmt.executeBatch();
     }
 }
