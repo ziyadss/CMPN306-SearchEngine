d2f8f0dd1692fe218b7c556ae16f0d8f8e35735e
Khaled Ibrahim <khaledashrafabdulrahman@gmail.com>
2022-05-17 23:40:46 +0200

    Schema changes

--- src/main/java/com/cmpn306/Main.java
+++ src/main/java/com/cmpn306/Main.java
@@ -1,7 +1,13 @@
 package com.cmpn306;
 
+import com.cmpn306.database.Database;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, IOException {
         System.out.println("Hello world!");
+        Database db = new Database();
     }
 }
