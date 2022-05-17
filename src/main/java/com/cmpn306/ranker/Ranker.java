e9351b528f8995d40f137eb8ea1762460a3d5bc1
Ziyad <ziyad.ss@hotmail.com>
2022-05-17 21:28:22 +0200

    Update Ranker.java

--- src/main/java/com/cmpn306/ranker/Ranker.java
+++ src/main/java/com/cmpn306/ranker/Ranker.java
@@ -1,3 +1,11 @@
 package com.cmpn306.ranker;
 
-public class Ranker { }
+import com.cmpn306.queryprocessor.QueryProcessor;
+
+import java.util.List;
+
+public class Ranker {
+    public static void rank(List<String> tokensList, List<QueryProcessor.QueryResult> results) {
+
+    }
+}
