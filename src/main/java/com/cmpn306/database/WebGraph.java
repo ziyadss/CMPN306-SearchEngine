876f10f5b2ba3db95eab6256a1470557a5e144e4
Ziyad <ziyad.ss@hotmail.com>
2022-05-19 08:33:23 +0200

    WebGraph migrated to new DB Interface
    Co-Authored-By: Khaled Ibrahim  <65508021+Huchra@users.noreply.github.com>
    Co-Authored-By: Ahmed Ayman Saad <34175435+AhmedAymanSaad@users.noreply.github.com>

--- src/main/java/com/cmpn306/database/WebGraph.java
+++ src/main/java/com/cmpn306/database/WebGraph.java
@@ -9,24 +9,40 @@ import java.util.Hashtable;
 public class WebGraph {
     Hashtable<String, WebGraphNode> docs = new Hashtable<>();
 
+    private Boolean documentProcess(ResultSet rs) {
+        try {
+            String srcDocUrl = rs.getString("srcDocUrl");
+            String dstDocUrl = rs.getString("dstDocUrl");
+            docs.get(srcDocUrl).outGoingUrls.put(dstDocUrl, docs.get(dstDocUrl));
+            docs.get(dstDocUrl).incomingUrls.put(srcDocUrl, docs.get(srcDocUrl));
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public Hashtable<String, WebGraphNode> getDocs() {
         return docs;
     }
 
+    private Boolean urlProcess(ResultSet rs) {
+        try {
+            // TODO: add pageRank to the node
+            String docUrl = rs.getString("docUrl");
+            //double       pageRank = rs.getDouble("pageRank");
+            WebGraphNode tmp_node = new WebGraphNode(docUrl, 1.0 / Ranker.getTotalDocCount());
+            docs.put(tmp_node.getDocUrl(), tmp_node);
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public void getDocumentsUrl() throws SQLException {
-        String                          query = "SELECT docUrl, pageRank FROM documents";
-        Hashtable<String, WebGraphNode> documents;
-        try (ResultSet rs = Database.INSTANCE.query(query)) {
-            documents = new Hashtable<>();
-            while (rs.next()) {
-                String       docUrl   = rs.getString("docUrl");
-                //double       pageRank = rs.getDouble("pageRank");
-                WebGraphNode tmp_node = new WebGraphNode(docUrl, 1.0/ Ranker.getTotalDocCount());
-                documents.put(tmp_node.getDocUrl(), tmp_node);
-            }
-        }
-        docs = documents;
+        String query = "SELECT docUrl, pageRank FROM documents";
+
+        Database.INSTANCE.query(query, this::urlProcess);
+
     }
 
     public void getDocumentsLinks() throws SQLException {
@@ -34,13 +50,7 @@ public class WebGraph {
         // "SELECT srcDocUrl,d1.pageRank as srcPageRank, d2.pageRank,dstDocUrl AS dstPageRank FROM web_graph"+
         //                                    "JOIN documents AS d1 ON web_graph.srcDocUrl = d1.docUrl" +
         //                                    "JOIN documents AS d2 ON web_graph.dstDocUrl = d2.docUrl";
-        try (ResultSet rs = Database.INSTANCE.query(query)) {
-            while (rs.next()) {
-                String srcDocUrl = rs.getString("srcDocUrl");
-                String dstDocUrl = rs.getString("dstDocUrl");
-                docs.get(srcDocUrl).outGoingUrls.put(dstDocUrl, docs.get(dstDocUrl));
-                docs.get(dstDocUrl).incomingUrls.put(srcDocUrl, docs.get(srcDocUrl));
-            }
-        }
+
+        Database.INSTANCE.query(query, this::documentProcess);
     }
 }
