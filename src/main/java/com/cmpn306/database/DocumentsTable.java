eab9fa690bbec74fd3d6307b598c7b6221c76fe7
Ziyad <ziyad.ss@hotmail.com>
2022-05-19 04:18:31 +0200

    Cleanup
    Added files to gitignore
    Formatted files
    Removed redundant type specifiers
    Removed redundant control-flow keywords (trailing return/continue)
    Moved closable objects to try-with-resources blocks

--- src/main/java/com/cmpn306/database/DocumentsTable.java
+++ src/main/java/com/cmpn306/database/DocumentsTable.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class DocumentsTable {
 
-	//deprecated, for testing only
+    //deprecated, for testing only
     public void selectUrl() throws SQLException {
         String query = "SELECT * FROM documents";
         try (ResultSet rs = Database.INSTANCE.query(query)) {
@@ -17,22 +17,22 @@ public class DocumentsTable {
         }
     }
 
-
-
     public List<Document> getIndexable(Integer limit) throws SQLException {
         String query = "SELECT docUrl, content, timeCurrent, pageRank, pageTitle FROM documents WHERE indexTime < timeCurrent limit " + limit;
 
-        ResultSet      rs        = Database.INSTANCE.query(query);
-        List<Document> documents = new ArrayList<Document>();
+        List<Document> documents;
+        try (ResultSet rs = Database.INSTANCE.query(query)) {
+            documents = new ArrayList<>();
 
-        while (rs.next()) {
-            String   docUrl      = rs.getString("docUrl");
-            String   content     = rs.getString("content");
-            int      wordCount   = 0;
-            long     timeCurrent = rs.getLong("timeCurrent");
-            float    pageRank    = rs.getFloat("pageRank");
-            String   pageTitle   = rs.getString("pageTitle");
-            Document tmp_doc     = new Document(docUrl, content, wordCount, timeCurrent, pageRank, pageTitle);
+            while (rs.next()) {
+                String   docUrl      = rs.getString("docUrl");
+                String   content     = rs.getString("content");
+                int      wordCount   = 0;
+                long     timeCurrent = rs.getLong("timeCurrent");
+                float    pageRank    = rs.getFloat("pageRank");
+                String   pageTitle   = rs.getString("pageTitle");
+                Document tmp_doc     = new Document(docUrl, content, wordCount, timeCurrent, pageRank, pageTitle);
+            }
         }
         return documents;
     }
@@ -43,17 +43,16 @@ public class DocumentsTable {
         Database.INSTANCE.update(query);
     }
 
-	public void updateIndexTime(List<Document> documents) throws SQLException {
-		StringBuilder query = new StringBuilder("UPDATE documents SET indexTime = " + System.currentTimeMillis() + " WHERE docUrl in (");
+    public void updateIndexTime(List<Document> documents) throws SQLException {
+        StringBuilder query = new StringBuilder("UPDATE documents SET indexTime = " + System.currentTimeMillis() + " WHERE docUrl in (");
 
-		for (int i = 0; i < documents.size(); ++i) {
-			query.append(documents.get(i).getDocUrl());
-			if (i != documents.size() - 1)
-				query.append(", ");
-		}
-		query.append(");");
+        for (int i = 0; i < documents.size(); ++i) {
+            query.append(documents.get(i).getDocUrl());
+            if (i != documents.size() - 1)
+                query.append(", ");
+        }
+        query.append(");");
 
-		Database.INSTANCE.update(query.toString());
-		return;
-	}
+        Database.INSTANCE.update(query.toString());
+    }
 }
--- src/main/java/com/cmpn306/database/WebGraphNode.java
+++ src/main/java/com/cmpn306/database/WebGraphNode.java
@@ -3,11 +3,12 @@ package com.cmpn306.database;
 import java.util.Hashtable;
 
 public class WebGraphNode {
-    String docUrl;
-    float pageRank;
-    Hashtable<String,WebGraph> outGoingUrls;
-    public WebGraphNode(String docUrl,float pageRank){
-        this.docUrl = docUrl;
+    String                      docUrl;
+    float                       pageRank;
+    Hashtable<String, WebGraph> outGoingUrls;
+
+    public WebGraphNode(String docUrl, float pageRank) {
+        this.docUrl   = docUrl;
         this.pageRank = pageRank;
     }
 
